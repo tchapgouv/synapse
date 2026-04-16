1d1348bf3a7d6a2efcebe54443cc35c9104f83fb
Mathieu Velten <mathieu@velten.xyz>
2026-04-16 17:48:34 +0200

    Fix lock retry interval bug

--- synapse/handlers/worker_lock.py
+++ synapse/handlers/worker_lock.py
@@ -232,6 +232,7 @@ class WaitingLock:
         """Release the lock (by resolving the deferred)"""
         if not self.deferred.called:
             with PreserveLoggingContext():
+                self._retry_interval = 0.1
                 self.deferred.callback(None)
 
     async def __aenter__(self) -> None:
@@ -347,6 +348,7 @@ class WaitingMultiLock:
         """Release the lock (by resolving the deferred)"""
         if not self.deferred.called:
             with PreserveLoggingContext():
+                self._retry_interval = 0.1
                 self.deferred.callback(None)
 
     async def __aenter__(self) -> None:
