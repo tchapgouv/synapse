9bdf180c315896688b320271284293444ddb0e6c
Olivier D <odelcroi@gmail.com>
2026-06-03 15:16:23 +0200

    copy im.vector.room.access_rules when upgrading (#10)

--- synapse/handlers/room.py
+++ synapse/handlers/room.py
@@ -616,6 +616,10 @@ class RoomCreationHandler:
             (EventTypes.PowerLevels, ""),
         ]
 
+        #:tchap: copy room access rule in the upgraded room
+        types_to_copy.extend([('im.vector.room.access_rules', "")])
+        #:tchap:end
+
         room_type = old_room_create_event.content.get(EventContentFields.ROOM_TYPE)
         if room_type is not None:
             # If the old room was a space, copy over the rooms in the space.
